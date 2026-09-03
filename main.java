691d29a428cc58a2ba830e9134a6401faaea9742
Advay Iyer <ariyer@vt.edu>
2026-09-03 16:04:25 -0400

    modified:   main.java added basic methods

--- main.java
+++ main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Player playerX = new Player("Player 1", Mark.X);
+        Player playerO = new Player("Player 2", Mark.O);
+        Game game = new Game(playerX, playerO);
     }
+    public static int getPos(int x, int y){
+        int posX = 0;
+        int posY = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (posX < 1 || posX > 9 || posY < 1 || posY > 9) {
+            System.out.print("Enter a position (1-9): ");
+            posX = scanner.nextInt();
+            posY = scanner.nextInt();
+            try {
+                if (posX < 1 || posX > 9 || posY < 1 || posY > 9) {
+                    throw new IllegalArgumentException("Position must be between 1 and 9.");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            return pos;
+        } 
+    public static void smallGame(){
+
+
+
+
+    }
+
 }
