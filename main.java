00c4e23d80c17877cf3c73ea89850597d31fa9fa
Advay Iyer <ariyer@vt.edu>
2026-09-10 14:08:34 -0400

    modified:   main.java

--- main.java
+++ main.java
@@ -7,23 +7,23 @@ public class main {
         Player playerO = new Player("Player 2", Mark.O);
         Game game = new Game(playerX, playerO);
     }
-    public static int getPos(int x, int y){
+    public static int getPos(int x){
         int posX = 0;
         int posY = 0;
         Scanner scanner = new Scanner(System.in);
-        while (posX < 1 || posX > 9 || posY < 1 || posY > 9) {
-            System.out.print("Enter a position (1-9): ");
+        while (posX < 1 || posX > 9){
+            System.out.print("Player 1, enter a position (1-9): ");
             posX = scanner.nextInt();
-            posY = scanner.nextInt();
             try {
-                if (posX < 1 || posX > 9 || posY < 1 || posY > 9) {
+                if (posX < 1 || posX > 9) {
                     throw new IllegalArgumentException("Position must be between 1 and 9.");
                 }
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
-            return pos;
-        } 
+        }
+        return posX;
+    }
     public static void smallGame(){
 
 
