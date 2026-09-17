2c1def7f13d7faf9693f355a87e8707e759df15f
Advay Iyer <ariyer@vt.edu>
2026-09-17 15:07:04 -0400

    rpstestcreation

---
+++ RPStest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RPSTest {
+    @Test
+    void testGame() {
+        RPS game = new RPS();
+        
+    }
+}
